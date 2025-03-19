5ca2ab4e12aaa716255c7f9f796fe4809df6f2bf
abhishek <bhshkvijayan@gmail.com>
2025-03-19 14:11:12 +0530

    first commit

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://abhishek8102tech:YV0QKYaeiHvDKCPP@cluster0.9aqpa.mongodb.net/userDB", {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect("mongodb+srv://abhishek8102tech:YV0QKYaeiHvDKCPP@cluster0.9aqpa.mongodb.net/userDB");
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDB connection failed:", error);
